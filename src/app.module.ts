a0074c369d7aa9dceba30cd40ca5eeb3ebe0f36b
Kaung Myat Han <arkarhtethan@gmail.com>
2021-08-05 18:11:19 +0630

    typpeorm configuration added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { IdeaEntity } from './ideas/idea.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(
+      {
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'root',
+        password: 'root',
+        database: 'ideas',
+        synchronize: true,
+        logging: true,
+        entities: [IdeaEntity],
+      }
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
