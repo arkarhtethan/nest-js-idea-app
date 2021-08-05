5de8fea7b4ac4d74d107f2fea632c9ee10beaa12
Kaung Myat Han <arkarhtethan@gmail.com>
2021-08-06 01:02:58 +0630

    crud controller funtion added

--- src/idea/idea.controller.ts
+++ src/idea/idea.controller.ts
@@ -1,6 +1,40 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Logger } from '@nestjs/common/services/logger.service';
+import { IdeaDTO } from './idea.dto';
+import { IdeaEntity } from './idea.entity';
+import { IdeaService } from './idea.service';
 
 @Controller('idea')
 export class IdeaController {
+    constructor(private ideaServie: IdeaService) { }
 
+    @Get()
+    showAllIdea (): Promise<IdeaEntity[]> {
+        return this.ideaServie.showAll();
+    }
+
+    @Post()
+    createIdea (@Body() idea: IdeaDTO) {
+        return this.ideaServie.create(idea);
+    }
+
+    @Get(':id')
+    readIdea (@Param('id') id: string): Promise<IdeaEntity> {
+        return this.ideaServie.read(id);
+    }
+
+    @Put(':id')
+    updateIdea (
+        @Param('id') id: string,
+        @Body() data: Partial<IdeaEntity>
+    ): Promise<IdeaEntity> {
+        return this.ideaServie.update(id, data);
+    }
+
+    @Delete(':id')
+    destroyIdea (@Param('id') id: string) {
+        return this.ideaServie.destroy(id);
+    }
 }
